665ce6f687e193a09f52f49e0035cd6adc35695b
Victor Van den Broeck <victorvdb@outlook.com>
2018-04-26 21:28:30 +0200

    Firebase login connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAjk9cg2-F1qC1Wpmj-uWX5pY4HFzm30Vo',
+    authDomain: 'gallery-victorvdb.firebaseapp.com',
+    databaseURL: 'https://gallery-victorvdb.firebaseio.com',
+    projectId: 'gallery-victorvdb',
+    storageBucket: 'gallery-victorvdb.appspot.com',
+    messagingSenderId: '111393832285'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAjk9cg2-F1qC1Wpmj-uWX5pY4HFzm30Vo',
+    authDomain: 'gallery-victorvdb.firebaseapp.com',
+    databaseURL: 'https://gallery-victorvdb.firebaseio.com',
+    projectId: 'gallery-victorvdb',
+    storageBucket: 'gallery-victorvdb.appspot.com',
+    messagingSenderId: '111393832285'
+  }
 };
